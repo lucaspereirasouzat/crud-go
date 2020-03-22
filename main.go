3815321d7a6648f68ea23155243087f6e819793f
Warmachine13 <lucaspereiradesouzat@gmail.com>
2020-03-22 18:11:50 -0300

    iniciar o servidor e escutar o get

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "bem vindo")
+	})
+	// iniciar servidor na porta localhost:1337
+	println("Servidor iniciado na porta 1337")
+	http.ListenAndServe(":1337", nil)
+}
