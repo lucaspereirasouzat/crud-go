dfd807921e7b928c9563210a940fa40fb76e8e10
Warmachine13 <lucaspereiradesouzat@gmail.com>
2020-03-25 00:29:10 -0300

    new

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
-<<<<<<< HEAD
 // imports principais
-=======
->>>>>>> 3815321d7a6648f68ea23155243087f6e819793f
 import (
 	"fmt"
 	"net/http"
@@ -11,17 +8,10 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-<<<<<<< HEAD
 		// resposta para o front
-=======
->>>>>>> 3815321d7a6648f68ea23155243087f6e819793f
 		fmt.Fprintf(w, "bem vindo")
 	})
 	// iniciar servidor na porta localhost:1337
 	println("Servidor iniciado na porta 1337")
-<<<<<<< HEAD
-
-=======
->>>>>>> 3815321d7a6648f68ea23155243087f6e819793f
 	http.ListenAndServe(":1337", nil)
 }
